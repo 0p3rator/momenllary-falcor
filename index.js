cbc955620cb62d0855aa38f9d7e0fa610a14f261
wangchangyu <wangchy1128@gmail.com>
2018-02-06 11:40:22 +0800

    change from 127.0.0.1 to 0.0.0.0

--- index.js
+++ index.js
@@ -83,4 +83,4 @@ app.use('/model.json', cors(), falcorExpress.dataSourceRoute(function (req, res)
 // serve static files from current directory
 app.use(express.static(__dirname + '/'));
 
-var server = app.listen(3300);
+var server = app.listen(3300,'0.0.0.0');
